b6187b75d40ef6ff436a24fa1686c1c89531a6ef
Imasha Sandanayaka <imashasandanayaka@gmail.com>
2025-03-13 16:40:15 +0530

    hash user password🔐

--- backend/server/src/model/User.js
+++ backend/server/src/model/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 //schema
 const userSchema = mongoose.Schema({
@@ -29,6 +30,17 @@ const userSchema = mongoose.Schema({
     
   }
 );
+
+userSchema.pre("save", async function (next){
+    if(!this.isModified("password")){
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
 
 //compile schema into model
 const User = mongoose.model('User', userSchema);
