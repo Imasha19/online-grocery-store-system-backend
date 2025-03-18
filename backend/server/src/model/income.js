cdaaa69bfd1afb29be0226e6996c2e28eb570c87
Imasha Sandanayaka <imashasandanayaka@gmail.com>
2025-03-18 14:26:00 +0530

    add current date to income

--- backend/server/src/model/income.js
+++ backend/server/src/model/income.js
@@ -3,40 +3,41 @@ const mongoosePaginate = require('mongoose-paginate-v2');
 
 //schema
 const incomeSchema = mongoose.Schema({
-    title:{
-        required:[true,"Title is required"],
-        type:String,
+    title: {
+        required: [true, "Title is required"],
+        type: String,
     },
-    description:{
-        required:[true,"Description is required"],
-        type:String,
+    description: {
+        required: [true, "Description is required"],
+        type: String,
     },
-    type:{
-        type:String,
-        default:"Income",
+    type: {
+        type: String,
+        default: "Income",
     },
-    amount:{
-        required:[true,"Amount is required"],
-        type:Number,
+    amount: {
+        required: [true, "Amount is required"],
+        type: Number,
     },
-   user:{
-         type:mongoose.Schema.Types.ObjectId,
-         ref:"User",
-         required:[true,"User ID is required"],
-   },
-   
-  },
-  {
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: [true, "User ID is required"],
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+}, {
     timestamps: true,
-    toJSON:{
-        virtuals:true,
+    toJSON: {
+        virtuals: true,
     },
-    toObject:{
-        virtuals:true,
+    toObject: {
+        virtuals: true,
     }
-    
-  }
-);
+});
+
 //pagination
 incomeSchema.plugin(mongoosePaginate);
 
